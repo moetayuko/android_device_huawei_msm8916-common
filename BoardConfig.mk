fce1d3265acacaa234d5c66baf0ef6d9c82cf681
dianlujitao <dianlujitao@gmail.com>
2015-07-05 17:02:09 +0800

    msm8916-common: Set selinux to permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Kernel
 TARGET_NO_KERNEL := false
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
