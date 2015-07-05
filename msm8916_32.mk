570f21402952ae1f79ba8742057a4457cff67992
dianlujitao <dianlujitao@gmail.com>
2015-07-05 10:16:43 +0800

    msm8916-common: Merge device/qcom/common

--- msm8916_32.mk
+++ msm8916_32.mk
@@ -35,6 +35,29 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+
+# gps/location secuity configuration file
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sec_config:system/etc/sec_config
+
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
@@ -72,29 +95,179 @@ endif # TARGET_USES_QCA_NFC
 PRODUCT_PACKAGES += \
     AntHalService \
     libantradio \
-    antradio_app
+    antradio_app \
+    com.dsi.ant.antradio_library
 
+# APPOPS_POLICY
 PRODUCT_PACKAGES += \
+    appops_policy.xml
+
+# Audio
+PRODUCT_PACKAGES += \
+    audiod \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio.primary.msm8916 \
+    audio_policy.msm8916 \
+    tinymix \
     libqcomvisualizer \
     libqcompostprocbundle \
     libqcomvoiceprocessing
 
+# Charger
 PRODUCT_PACKAGES += \
+    charger_res_images
+
+# CRDA
+PRODUCT_PACKAGES += \
+    crda \
+    regdbdump \
+    regulatory.bin \
+    linville.key.pub.pem \
+    init.crda.sh
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs \
+    e2fsck
+
+# FM
+PRODUCT_PACKAGES += \
+    qcom.fmradio \
+    libqcomfm_jni \
+    FM2 \
+    FMRecord
+
+# Graphics
+PRODUCT_PACKAGES += \
+    copybit.msm8916 \
+    gralloc.msm8916 \
+    memtrack.msm8916 \
+    hwcomposer.msm8916 \
+    libtinyxml
+
+# INIT
+PRODUCT_PACKAGES += \
+    init.qcom.modem_links.sh \
+    init.target.rc \
+    init.qti.ims.sh \
+    init.qcom.bt.sh \
+    hsic.control.bt.sh \
+    init.qcom.coex.sh \
+    init.qcom.fm.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.syspart_fixup.sh \
+    init.qcom.rc \
+    init.qcom.factory.rc \
+    init.qcom.sdio.sh \
+    init.qcom.sh \
+    init.qcom.class_core.sh \
+    init.class_main.sh \
+    init.qcom.wifi.sh \
+    vold.fstab \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.qcom.efs.sync.sh \
+    ueventd.qcom.rc \
+    init.ath3k.bt.sh \
+    qca6234-service.sh \
+    init.qcom.audio.sh \
+    init.mdm.sh \
+    init.qcom.uicc.sh \
+    fstab.qcom \
+    init.qcom.debug.sh \
+    init.qcom.zram.sh \
+    init.qcom.bms.sh \
+    hcidump.sh
+
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8916
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
+
+# KEYPAD
+PRODUCT_PACKAGES += \
+    synaptics_rmi4_i2c.kl \
+    synaptics_dsx.kl \
+    ft5x06_ts.kl \
+    gpio-keys.kl
+
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libmm-omxcore \
+    libOmxCore \
+    libdivxdrmdecrypt \
+    libOmxSwVencMpeg4 \
+    libOmxVdec \
+    libOmxVdecHevc \
+    libOmxVdpp \
+    libOmxVenc \
+    libOmxVidEnc \
+    libstagefrighthw \
+    libdashplayer \
+    qcmediaplayer
+
+# Power
+PRODUCT_PACKAGES += \
+    power.msm8916
+
+# Ril
+PRODUCT_PACKAGES += \
+    libxml2
+
+# Stk
+PRODUCT_PACKAGES += \
+    Stk
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# WiFi
+PRODUCT_PACKAGES += \
+    wpa_supplicant.conf \
+    wpa_supplicant \
+    libwpa_client \
+    wcnss_service \
+    libwcnss_qmi \
+    libQWiFiSoftApCfg \
+    libqsap_sdk \
     wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapminfree=6m \
-    dalvik.vm.heapstartsize=14m
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
+    p2p_supplicant_overlay.conf \
+    hostapd \
+    hostapd_cli \
+    hostapd.conf \
+    hostapd.deny \
+    hostapd.accept
 
 PRODUCT_BOOT_JARS += \
     qcmediaplayer \
-    WfdCommon \
-    oem-services \
-    qcom.fmradio \
-    org.codeaurora.Performance \
-    tcmiface
+    qcom.fmradio
+
+# System Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapminfree=6m \
+    dalvik.vm.heapstartsize=14m \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.sib16_support=1 \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config \
+    ro.sf.lcd_density=320
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp \
+    camera2.portability.force_api=1
